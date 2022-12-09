f0f252182ca393b902b4fdb4ac2d4610de426a03
Amanda Zhang <amandazhanglele@gmail.com>
2022-12-08 18:30:27 -0800

    points plotted

--- map/app.R
+++ map/app.R
@@ -1,37 +1,47 @@
-# install.packages("leaflet") # Download and install leaflet package
-library(leaflet) # Loads the leaflet library for use
+# install.packages("leaflet")
+library(leaflet)
 
 runs2 <- read.csv("https://raw.githubusercontent.com/amndazhang/project-dynamar-map/master/Processed_GPE3_Tracks_BUM_SAL.csv")
 unique_fish <- unique(runs2$species)
 
-# Call the leaflet package to setup the maps and add the stops
-m <- leaflet() %>%
-  addProviderTiles("CartoDB.Positron") # Add the map tiles
-
- #For each unique run number, map the route
+m <- leaflet() %>% addProviderTiles("CartoDB.Positron")
 
 for (i in 1:length(unique_fish)) {
-  run_species <- unique_fish[i] # Get the run number.
+  run_species <- unique_fish[i]
   m <- addPolylines(m, lng=runs2[runs2$species == run_species,"lon"],
                     lat=runs2[runs2$species == run_species,"lat"],
                     color = ifelse(run_species=="BUM","blue","red"),
                     weight=1)
+  
+  m <- addCircles(m, lng=runs2[runs2$species == run_species,"lon"], 
+                  lat=runs2[runs2$species == run_species,"lat"], 
+                  color = "black", weight = 1, radius = 5)
 }
 
+#print(value)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-  
-  # Application title
-  titlePanel("Project DynaMAR Map"),
-  
-  # map projection
-  m
+  titlePanel("Project DynaMar Map"),
+
+  sidebarLayout(
+    
+    sidebarPanel(
+      checkboxGroupInput("display", "Display:", c("lines", "points")),
+      verbatimTextOutput("value")
+      #selectInput("tags", "Tag SN: ", unique(runs2$ptt), multiple = TRUE),
+      #tableOutput("data"),
+      #sliderInput("rng", "Depth Range", value = c(-2000, 0), min = -2000, max = 0)
+    ),
+    
+    mainPanel(
+      m
+    )
+  )
 )
 
-# Define server logic
-server <- function(input, output) {
-}
+server <- function(input, output, session) {
+  #output$value <- reactive({input$display})
 
-# Run the application 
+} 
+
 shinyApp(ui = ui, server = server)
