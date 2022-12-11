c596317610b1a44b0e5c6886c2a7a55dc09adfda
Amanda Zhang <amandazhanglele@gmail.com>
2022-12-10 22:55:00 -0800

    no depth range

--- app.R
+++ app.R
@@ -12,7 +12,6 @@ ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", width = "100%", height = "100%"),
   absolutePanel(top = 10, right = 10,
-                sliderInput("range", "Depth", value = c(-2000, 0), min = -2000, max = 0),
                 selectInput("tags", "Tag SN: ", unique(tracks$ptt), multiple = FALSE),
                 checkboxInput("points", "Show points", TRUE),
                 checkboxInput("lines", "Show paths", TRUE),
