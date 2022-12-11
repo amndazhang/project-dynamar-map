0e66241488da4cd4a64d66ee269a3e31cc2271e6
Amanda Zhang <amandazhanglele@gmail.com>
2022-12-10 22:49:50 -0800

    draft 1 done

--- app.R
+++ app.R
@@ -13,20 +13,13 @@ ui <- bootstrapPage(
   leafletOutput("map", width = "100%", height = "100%"),
   absolutePanel(top = 10, right = 10,
                 sliderInput("range", "Depth", value = c(-2000, 0), min = -2000, max = 0),
-                checkboxInput("all", "Show all", TRUE),
                 selectInput("tags", "Tag SN: ", unique(tracks$ptt), multiple = FALSE),
-                checkboxInput("legend", "Show legend", TRUE),
                 checkboxInput("points", "Show points", TRUE),
                 checkboxInput("lines", "Show paths", TRUE),
   )
 )
 
 server <- function(input, output, session) {
-  
-  # Update Tag SN input.
-  filteredData <- reactive({
-    tracks[tracks$ptt == input$tags,]
-  })
 
   output$map <- renderLeaflet({
     leaflet(tracks) %>% addTiles() %>%
@@ -35,19 +28,23 @@ server <- function(input, output, session) {
   
   # Update points and paths checkboxes.
   observe({
+    filteredData <- reactive({
+      tracks[tracks$ptt == input$tags,]
+    })
+    
     proxy <- leafletProxy("map", data = filteredData())
     proxy %>% clearShapes()
     
     if (input$lines) {
       proxy %>% addPolylines(lng = ~lon, lat = ~lat, color = "#ff9632", 
                              weight = 3, fillOpacity = 2, 
-                             label = ~htmlEscape(ptt))
+                             label = ~htmlEscape(species))
     }
     if (input$points) {
       proxy %>% addCircles(radius = 20, weight = 10, color = "#ff9632",
                            fillColor = "red", fillOpacity = 1, 
                            label = ~htmlEscape(date))
-    }  
+    }   
   })
   
 }
