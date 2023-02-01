e7907dd2263dbc07d09c326253e380c847a37a23
Amanda Zhang <amandazhanglele@gmail.com>
2023-01-31 23:56:06 -0800

    needs work

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(leaflet)
-library(RColorBrewer)
 library(htmltools)
+library(RColorBrewer)
 
 tracks <- read.csv("https://raw.githubusercontent.com/amndazhang/project-dynamar-map/master/Processed_GPE3_Tracks_BUM_SAL.csv")
 unique_fish <- unique(tracks$species)
@@ -12,43 +12,71 @@ ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", width = "100%", height = "100%"),
   absolutePanel(top = 10, right = 10,
-                selectInput("tags", "Tag SN: ", unique(tracks$ptt), multiple = FALSE),
+                checkboxInput("showAll", "Show All", TRUE),
+                selectInput("tags", "Tag SN: ", unique(tracks$ptt), multiple = TRUE),
                 checkboxInput("points", "Show points", TRUE),
                 checkboxInput("lines", "Show paths", TRUE),
   )
 )
 
 server <- function(input, output, session) {
-
+  
   output$map <- renderLeaflet({
     leaflet(tracks) %>% addTiles() %>%
       fitBounds(~min(lon), ~min(lat), ~max(lon), ~max(lat))
   })
   
-  # Update points and paths checkboxes.
   observe({
-    filteredData <- reactive({
-      tracks[tracks$ptt == input$tags,]
-    })
     
-    proxy <- leafletProxy("map", data = filteredData())
-    proxy %>% clearShapes()
-    
-    s <- tracks[tracks$ptt == input$tags,]$species
-    if (input$lines) {
-      proxy %>% addPolylines(lng = ~lon, lat = ~lat, 
-                             weight = 3, fillOpacity = 2, 
-                             label = ~htmlEscape(species),
-                             color = ifelse(s=="BUM", "#ff9632", "#d743d9"))
-    }
-    if (input$points) {
-      proxy %>% addCircles(radius = 20, weight = 10, 
-                           color = ifelse(s=="BUM", "#ff9632", "#d743d9"),
-                           fillColor = "red", fillOpacity = 1, 
-                           label = ~htmlEscape(date))
-    }   
-  })
-  
+    if (input$showAll) {
+      
+      s <- tracks[tracks$ptt, ]$species
+      c <- ifelse(s=="BUM", brewer.pal(9, "Blues"), brewer.pal(9, "Reds"))
+      output$map <- renderLeaflet({
+        leaflet(tracks) %>% addTiles() %>%
+          fitBounds(~min(lon), ~min(lat), ~max(lon), ~max(lat)) %>%
+          addCircles(lng = ~lon, lat = ~lat, weight = 2, radius = 5,
+                     label = ~htmlEscape(date)) %>%
+          addPolylines(lng = ~lon, lat = ~lat, 
+                       weight = 3, 
+                       label = ~htmlEscape(species),
+                       color = "#ff9632")
+      })
+      
+      # proxy <- leafletProxy("map", tracks)
+      # #s <- tracks[tracks$ptt, ]$species
+      # proxy %>% addPolylines(lng = ~lon, lat = ~lat, 
+      #                        weight = 3, fillOpacity = 2, 
+      #                        label = ~htmlEscape(species),
+      #                        color = "#ff9632") #ifelse(s=="BUM", "#ff9632", "#d743d9"))
+      # proxy %>% addCircles(radius = 20, weight = 10, 
+      #                      color = "#ff9632", #ifelse(s=="BUM", "#ff9632", "#d743d9"))
+      #                      fillColor = "red", fillOpacity = 1, 
+      #                      label = ~htmlEscape(date))
+    } 
+    # else {
+      
+    #   filteredData <- reactive({
+    #     tracks[tracks$ptt == input$tags,]
+    #   })
+    #   proxy <- leafletProxy("map", data = filteredData())
+    #   proxy %>% clearShapes()
+      
+    #   s <- tracks[tracks$ptt == input$tags,]$species
+    #   if (input$lines) {
+    #     proxy %>% addPolylines(lng = ~lon, lat = ~lat, 
+    #                            weight = 3, fillOpacity = 2, 
+    #                            label = ~htmlEscape(species),
+    #                            color = ifelse(s=="BUM", "#ff9632", "#d743d9"))
+    #   }
+    #   if (input$points) {
+    #     proxy %>% addCircles(radius = 20, weight = 10, 
+    #                          color = ifelse(s=="BUM", "#ff9632", "#d743d9"),
+    #                          fillColor = "red", fillOpacity = 1, 
+    #                          label = ~htmlEscape(date))
+    #   }   
+    # }
+  }) 
 }
 
 shinyApp(ui, server)
