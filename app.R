574f20207f3f27d7accd0b58da27bd24f959d7fb
Amanda Zhang <amandazhanglele@gmail.com>
2023-02-06 01:10:49 -0800

    color legend + new configs

--- app.R
+++ app.R
@@ -10,73 +10,33 @@ map <- leaflet() %>% addProviderTiles("CartoDB.Positron")
 
 ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-  leafletOutput("map", width = "100%", height = "100%"),
-  absolutePanel(top = 10, right = 10,
-                checkboxInput("showAll", "Show All", TRUE),
-                selectInput("tags", "Tag SN: ", unique(tracks$ptt), multiple = TRUE),
-                checkboxInput("points", "Show points", TRUE),
-                checkboxInput("lines", "Show paths", TRUE),
-  )
+  leafletOutput("map", width = "100%", height = "100%")
 )
 
 server <- function(input, output, session) {
   
   output$map <- renderLeaflet({
     leaflet(tracks) %>% addTiles() %>%
-      fitBounds(~min(lon), ~min(lat), ~max(lon), ~max(lat))
+    fitBounds(~min(lon), ~min(lat), ~max(lon), ~max(lat)) %>%
+    # Overlay groups
+    addCircles(lng = ~lon, lat = ~lat, weight = 2, radius = 5,
+               label = ~htmlEscape(date), group = "Points") %>%
+    addPolylines(lng = ~lon, lat = ~lat, weight = 2, group = "Outline") %>%
+    # Layers control
+    addLayersControl(
+      baseGroups = c("Show All", "Show Specific"),
+      overlayGroups = c("Points", "Outline"),
+      options = layersControlOptions(collapsed = FALSE)) %>%
+    addLegend("bottomright", 
+              colors = c("#ff4d00",  "#0033ff"),
+              labels = c("SAL", "BUM"),
+              title = "Species",
+              opacity = 1)
   })
-  
+
   observe({
-    
-    if (input$showAll) {
-      
-      s <- tracks[tracks$ptt, ]$species
-      c <- ifelse(s=="BUM", brewer.pal(9, "Blues"), brewer.pal(9, "Reds"))
-      output$map <- renderLeaflet({
-        leaflet(tracks) %>% addTiles() %>%
-          fitBounds(~min(lon), ~min(lat), ~max(lon), ~max(lat)) %>%
-          addCircles(lng = ~lon, lat = ~lat, weight = 2, radius = 5,
-                     label = ~htmlEscape(date)) %>%
-          addPolylines(lng = ~lon, lat = ~lat, 
-                       weight = 3, 
-                       label = ~htmlEscape(species),
-                       color = "#ff9632")
-      })
-      
-      # proxy <- leafletProxy("map", tracks)
-      # #s <- tracks[tracks$ptt, ]$species
-      # proxy %>% addPolylines(lng = ~lon, lat = ~lat, 
-      #                        weight = 3, fillOpacity = 2, 
-      #                        label = ~htmlEscape(species),
-      #                        color = "#ff9632") #ifelse(s=="BUM", "#ff9632", "#d743d9"))
-      # proxy %>% addCircles(radius = 20, weight = 10, 
-      #                      color = "#ff9632", #ifelse(s=="BUM", "#ff9632", "#d743d9"))
-      #                      fillColor = "red", fillOpacity = 1, 
-      #                      label = ~htmlEscape(date))
-    } 
-    # else {
-      
-    #   filteredData <- reactive({
-    #     tracks[tracks$ptt == input$tags,]
-    #   })
-    #   proxy <- leafletProxy("map", data = filteredData())
-    #   proxy %>% clearShapes()
-      
-    #   s <- tracks[tracks$ptt == input$tags,]$species
-    #   if (input$lines) {
-    #     proxy %>% addPolylines(lng = ~lon, lat = ~lat, 
-    #                            weight = 3, fillOpacity = 2, 
-    #                            label = ~htmlEscape(species),
-    #                            color = ifelse(s=="BUM", "#ff9632", "#d743d9"))
-    #   }
-    #   if (input$points) {
-    #     proxy %>% addCircles(radius = 20, weight = 10, 
-    #                          color = ifelse(s=="BUM", "#ff9632", "#d743d9"),
-    #                          fillColor = "red", fillOpacity = 1, 
-    #                          label = ~htmlEscape(date))
-    #   }   
-    # }
   }) 
+  
 }
 
 shinyApp(ui, server)
